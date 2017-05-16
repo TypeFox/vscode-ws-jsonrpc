789837b206577b18622847527866c3201ab8e08c
akosyakov <anton.kosyakov@typefox.io>
2017-05-16 10:54:11 +0200

    buffer ws events

--- src/socket/reader.ts
+++ src/socket/reader.ts
@@ -8,13 +8,18 @@ import { IWebSocket } from "./socket";
 
 export class WebSocketMessageReader extends AbstractStreamMessageReader {
 
+    protected state: 'initial' | 'listening' | 'closed' = 'initial';
+    protected callback: DataCallback | undefined;
+    protected readonly events: { message?: any, error?: any }[] = [];
+
     constructor(protected readonly socket: IWebSocket) {
         super();
-    }
-
-    listen(callback: DataCallback): void {
-        this.socket.onMessage(data => this.readMessage(data, callback));
-        this.socket.onError(reason => this.fireError(reason));
+        this.socket.onMessage(message =>
+            this.readMessage(message)
+        );
+        this.socket.onError(error =>
+            this.fireError(error)
+        );
         this.socket.onClose((code, reason) => {
             if (code !== 1000) {
                 const error: Error = {
@@ -23,8 +28,50 @@ export class WebSocketMessageReader extends AbstractStreamMessageReader {
                 };
                 this.fireError(error);
             }
-            this.fireClose();
+            super.fireClose();
         });
+    }
+
+    listen(callback: DataCallback): void {
+        if (this.state === 'initial') {
+            if (this.events.length !== 0) {
+                const event = this.events.pop()!;
+                if (event.message) {
+                    super.readMessage(event.message, callback);
+                } else if (event.error) {
+                    this.fireError(event.error);
+                } else {
+                    this.fireClose();
+                }
+            }
+            this.callback = callback;
+            this.state = 'listening';
+        }
+    }
+
+    protected readMessage(message: any): void {
+        if (this.state === 'initial') {
+            this.events.splice(0, 0, { message });
+        } else if (this.state === 'listening') {
+            super.readMessage(message, this.callback!);
+        }
+    }
+
+    protected fireError(error: any): void {
+        if (this.state === 'initial') {
+            this.events.splice(0, 0, { error });
+        } else if (this.state === 'listening') {
+            super.fireError(error);
+        }
+    }
+
+    protected fireClose(): void {
+        if (this.state === 'initial') {
+            this.events.splice(0, 0, {});
+        } else if (this.state === 'listening') {
+            super.fireClose();
+        }
+        this.state = 'closed';
     }
 
 }
