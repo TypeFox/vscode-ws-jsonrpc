a1aa6609978e66c793322128c4af3a7637dc73ee
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-05-22 11:20:27 +0200

    Added more connection factory methods
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- src/server/launch.ts
+++ src/server/launch.ts
@@ -2,19 +2,48 @@
  * Copyright (c) 2017 TypeFox GmbH (http://www.typefox.io). All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-import * as cp from 'child_process';
-import { StreamMessageReader, StreamMessageWriter } from "vscode-jsonrpc";
-import { IConnection, createConnection } from "./connection";
 
-export function createServerProcess(serverName: string, command: string, args?: string[]): IConnection {
-    const serverProcess = cp.spawn(command, args);
+import * as net from 'net';
+import * as stream from 'stream';
+import * as cp from 'child_process';
+import { StreamMessageReader, StreamMessageWriter, SocketMessageReader, SocketMessageWriter } from "vscode-jsonrpc";
+import { IConnection, createConnection } from "./connection";
+import { IWebSocket, WebSocketMessageReader, WebSocketMessageWriter } from '../socket';
+
+export function createServerProcess(serverName: string, command: string, args?: string[], options?: cp.SpawnOptions): IConnection {
+    const serverProcess = cp.spawn(command, args, options);
     serverProcess.on('error', error =>
         console.error(`Launching ${serverName} Server failed: ${error}`)
     );
     serverProcess.stderr.on('data', data =>
         console.error(`${serverName} Server: ${data}`)
     );
-    const reader = new StreamMessageReader(serverProcess.stdout);
-    const writer = new StreamMessageWriter(serverProcess.stdin);
-    return createConnection(reader, writer, () => serverProcess.kill());
+    return createProcessStreamConnection(serverProcess);
 }
+
+export function createWebSocketConnection(socket: IWebSocket): IConnection {
+    const reader = new WebSocketMessageReader(socket);
+    const writer = new WebSocketMessageWriter(socket);
+    return createConnection(reader, writer, () => socket.dispose());
+}
+
+export function createProcessSocketConnection(process: cp.ChildProcess, outSocket: net.Socket, inSocket: net.Socket = outSocket): IConnection {
+    return createSocketConnection(outSocket, inSocket, () => process.kill());
+}
+
+export function createSocketConnection(outSocket: net.Socket, inSocket: net.Socket, onDispose: () => void): IConnection {
+    const reader = new SocketMessageReader(outSocket);
+    const writer = new SocketMessageWriter(inSocket);
+    return createConnection(reader, writer, onDispose);
+}
+
+export function createProcessStreamConnection(process: cp.ChildProcess): IConnection {
+    return createStreamConnection(process.stdout, process.stdin, () => process.kill());
+}
+
+export function createStreamConnection(outStream: stream.Readable, inStream: stream.Writable, onDispose: () => void): IConnection {
+    const reader = new StreamMessageReader(outStream);
+    const writer = new StreamMessageWriter(inStream);
+    return createConnection(reader, writer, onDispose);
+}
+
